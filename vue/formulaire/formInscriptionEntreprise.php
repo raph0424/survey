99450592c88d59a9397a3e1a1aadf9211d90305f
kevin9499 <46714122+kevin9499@users.noreply.github.com>
2019-05-28 15:51:53 +0200

    Add files via upload

---
+++ vue/formulaire/formInscriptionEntreprise.php
@@ -0,0 +1,24 @@
+<center>
+  <form  class="form-group" action="" method="post">
+    <table>
+      <tr><td>Nom</td><td><input  class="form-control" type="text" name="nom"></td></tr>
+      <tr><td>Prenom</td><td><input  class="form-control" type="text" name="prenom"></td></tr>
+      <tr><td>Email</td><td><input   class="form-control"type="text" name="email"></td></tr>
+      <tr><td>Mdp</td><td><input  class="form-control" type="password" name="mdp"></td></tr>
+      <tr><td>Telephon</td><td><input  class="form-control" type="text" name="telephone"></td></tr>
+      <tr><td>Date de naissance</td><td><input  class="form-control" type="date" name="date_naissance"></td></tr>
+      <tr><td>Adresse</td><td><input  class="form-control" type="text" name="adresse"></td></tr>
+      <tr><td>Code postal</td><td><input   class="form-control" type="text" name="code_postal"></td></tr>
+      <tr><td>Role</td><td><select   class="form-control" name="role">
+                                <option value="admin">Administrateur</option>
+                                <option value="user">Utilisateur</option>
+                                <option value="partenaire">Partenaire</option>
+                          </select>
+      </td></tr>
+      <tr>
+                  <td><input class ='btn btn-primary'  type="submit" name="sinscrire" value="S'inscrire"></td>
+                  <td><input class ='btn btn-primary' type="reset" name="Annuler" value="Annuler"></td>
+      </tr>
+    </table>
+  </form>
+  </center>
