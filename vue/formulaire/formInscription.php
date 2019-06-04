d59f34a31347388f54150ac921a0486e9516cc84
kevin9499 <46714122+kevin9499@users.noreply.github.com>
2019-06-04 11:41:41 +0200

    Add files via upload

--- vue/formulaire/formInscription.php
+++ vue/formulaire/formInscription.php
@@ -1,6 +1,6 @@
 <link href="css/formstyle.css" rel="stylesheet">
 <style type="text/css">
-body {
+.body {
        background-color: #262626; 
 }
 .login-page {
@@ -8,7 +8,6 @@ body {
   padding: 8% 0 0;
   margin: auto;
 }
-
 .form {
   position: relative;
   z-index: 1;
@@ -19,7 +18,6 @@ body {
   text-align: center;
   box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24);
 }
-
 .form input {
   font-family: "Roboto", sans-serif;
   outline: 0;
@@ -31,7 +29,6 @@ body {
   box-sizing: border-box;
   font-size: 14px;
 }
-
 .form .btn {
   font-family: "Roboto", sans-serif;
   text-transform: uppercase;
@@ -46,33 +43,27 @@ body {
   transition: all 0.3 ease;
   cursor: pointer;
 }
-
 .form button:hover,
 .form button:active,
 .form button:focus {
   background: #43A047;
 }
-
 .form .message {
   margin: 15px 0 0;
   color: #b3b3b3;
   font-size: 12px;
 }
-
 .form .message a {
   color: #4CAF50;
   text-decoration: none;
 }
-
 .form .register-form {
   display: none;
 }
-
 .infoo {
   margin: 50px auto;
   text-align: center;
 }
-
 .infoo h1 {
   margin: 0 0 15px;
   padding: 0;
@@ -80,49 +71,45 @@ body {
   font-weight: 300;
   color: #1a1a1a;
 }
-
 .infoo span {
   color: #4d4d4d;
   font-size: 12px;
 }
-
 .infoo span a {
   color: #000000;
   text-decoration: none;
 }
-
 .infoo span .fa {
   color: #EF3B3A;
 }
 </style>
-<body>
+<div class = "body">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 <div id="login_page" class="login-page">
   <div class="form">
     <form id="register_form" class="register-form">
       <input type="text" name="nom" placeholder="Nom">
       <input type="text" name="prenom" placeholder="Prénom">
-      <input type="text" name="email" placeholder="email address" />
+      <input type="text" name="email" placeholder="Adresse email" />
       <input type="password" name="mdp" placeholder="password" />
       <input type="text" name="telephone" placeholder="N° Télephone" />
       <input type="date" name="date_naissance" placeholder="Votre date de naissance">
       <input type="text" name="adresse" placeholder="Adresse">
       <input type="text" name="code_postal" placeholder="Code postal">
       <input type="hidden" name="role" value="ROLE_USER" />
-      <input type="submit" class="btn" value="Sign up" />
+      <input type="submit" class="btn" name ="sinscrir" value="S'inscrire" />
       <p class="message">
           Désirez vous faire partie de nos partenaires ?
         <a href="#">Inscrivez vous en tant que partenaire</a>
-        
       </p>
     </form>
     <form id="login_form" action="#" method="POST" class="login-form">
-      <input name="acronyme" id="acronyme" type="text" placeholder="Acronyme de l'entreprise" />
+      <input name="accronyme" id="acronyme" type="text" placeholder="Acronyme de l'entreprise" />
       <input name="nom_marque" id="nom_marque" type="text" placeholder="Nom de votre Marque" />
-      <input name="password" id="password" type="password" placeholder="password" />
-      <input type="datetime-local" id="date_debut" name="date_debut" />
+      <input name="mdp" id="password" type="password" placeholder="password" />
+      <input type="date" id="date_debut" name="date_debut" />
       <input type="text"  id="adresse" name="adresse" placeholder="Adresse">
-      <input id="btnLg" type="submit" class="btn" value="Sign up" />
+      <input id="btnLg" type="submit" class="btn" name ="sinscrire" value="S'inscrire" />
       <p class="message">
         Voulez vous créer un simple compte utilisateur ?
         <a href="#">Aller au formulaire d'inscription</a>
@@ -130,7 +117,7 @@ body {
     </form>
   </div>
 </div>
-</body>
+</div>
 <script>
 $('.message a').click(function() {
   $('form').animate({
