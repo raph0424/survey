565e227d528b59fd50ba581c5748507b808427cc
Raph0424 <raphael0424@gmail.com>
2019-06-04 09:00:19 +0200

    formulaire inscription

--- vue/formulaire/formInscription.php
+++ vue/formulaire/formInscription.php
@@ -1,24 +1,136 @@
-<center>
-  <form  class="form-group" action="" method="post">
-    <table>
-      <tr><td>Nom</td><td><input  class="form-control" type="text" name="nom"></td></tr>
-      <tr><td>Prenom</td><td><input  class="form-control" type="text" name="prenom"></td></tr>
-      <tr><td>Email</td><td><input   class="form-control"type="text" name="email"></td></tr>
-      <tr><td>Mdp</td><td><input  class="form-control" type="password" name="mdp"></td></tr>
-      <tr><td>Telephon</td><td><input  class="form-control" type="text" name="telephone"></td></tr>
-      <tr><td>Date de naissance</td><td><input  class="form-control" type="date" name="date_naissance"></td></tr>
-      <tr><td>Adresse</td><td><input  class="form-control" type="text" name="adresse"></td></tr>
-      <tr><td>Code postal</td><td><input   class="form-control" type="text" name="code_postal"></td></tr>
-      <tr><td>Role</td><td><select   class="form-control" name="role">
-                                <option value="admin">Administrateur</option>
-                                <option value="user">Utilisateur</option>
-                                <option value="partenaire">Partenaire</option>
-                          </select>
-      </td></tr>
-      <tr>
-                  <td><input class ='btn btn-primary'  type="submit" name="sinscrire" value="S'inscrire"></td>
-                  <td><input class ='btn btn-primary' type="reset" name="Annuler" value="Annuler"></td>
-      </tr>
-    </table>
-  </form>
-  </center>
+<link href="css/formstyle.css" rel="stylesheet">
+<style type="text/css">
+.login-page {
+  width: 360px;
+  padding: 8% 0 0;
+  margin: auto;
+}
+
+.form {
+  position: relative;
+  z-index: 1;
+  background: #FFFFFF;
+  max-width: 360px;
+  margin: 0 auto 100px;
+  padding: 45px;
+  text-align: center;
+  box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24);
+}
+
+.form input {
+  font-family: "Roboto", sans-serif;
+  outline: 0;
+  background: #f2f2f2;
+  width: 100%;
+  border: 0;
+  margin: 0 0 15px;
+  padding: 15px;
+  box-sizing: border-box;
+  font-size: 14px;
+}
+
+.form .btn {
+  font-family: "Roboto", sans-serif;
+  text-transform: uppercase;
+  outline: 0;
+  background: #4CAF50;
+  width: 100%;
+  border: 0;
+  padding: 15px;
+  color: #FFFFFF;
+  font-size: 14px;
+  -webkit-transition: all 0.3 ease;
+  transition: all 0.3 ease;
+  cursor: pointer;
+}
+
+.form button:hover,
+.form button:active,
+.form button:focus {
+  background: #43A047;
+}
+
+.form .message {
+  margin: 15px 0 0;
+  color: #b3b3b3;
+  font-size: 12px;
+}
+
+.form .message a {
+  color: #4CAF50;
+  text-decoration: none;
+}
+
+.form .register-form {
+  display: none;
+}
+
+.infoo {
+  margin: 50px auto;
+  text-align: center;
+}
+
+.infoo h1 {
+  margin: 0 0 15px;
+  padding: 0;
+  font-size: 36px;
+  font-weight: 300;
+  color: #1a1a1a;
+}
+
+.infoo span {
+  color: #4d4d4d;
+  font-size: 12px;
+}
+
+.infoo span a {
+  color: #000000;
+  text-decoration: none;
+}
+
+.infoo span .fa {
+  color: #EF3B3A;
+}
+</style>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<div id="login_page" class="login-page">
+  <div class="form">
+    <form id="register_form" class="register-form">
+      <input type="text" name="nom" placeholder="Nom">
+      <input type="text" name="prenom" placeholder="Prénom">
+      <input type="text" name="email" placeholder="email address" />
+      <input type="password" name="mdp" placeholder="password" />
+      <input type="text" name="telephone" placeholder="N° Télephone" />
+      <input type="date" name="date_naissance" placeholder="Votre date de naissance">
+      <input type="text" name="adresse" placeholder="Adresse">
+      <input type="text" name="code_postal" placeholder="Code postal">
+      <input type="hidden" name="role" value="ROLE_USER" />
+      <input type="submit" class="btn" value="Sign up" />
+      <p class="message">
+          Désirez vous faire partie de nos partenaires ?
+        <a href="#">Inscrivez vous en tant que partenaire</a>
+        
+      </p>
+    </form>
+    <form id="login_form" action="#" method="POST" class="login-form">
+      <input name="acronyme" id="acronyme" type="text" placeholder="Acronyme de l'entreprise" />
+      <input name="nom_marque" id="nom_marque" type="text" placeholder="Nom de votre Marque" />
+      <input name="password" id="password" type="password" placeholder="password" />
+      <input type="datetime-local" id="date_debut" name="date_debut" />
+      <input type="text"  id="adresse" name="adresse" placeholder="Adresse">
+      <input id="btnLg" type="submit" class="btn" value="Sign up" />
+      <p class="message">
+        Voulez vous créer un simple compte utilisateur ?
+        <a href="#">Aller au formulaire d'inscription</a>
+      </p>
+    </form>
+  </div>
+</div>
+<script>
+$('.message a').click(function() {
+  $('form').animate({
+    height: "toggle",
+    opacity: "toggle"
+  }, "slow");
+});
+   </script>
--- vue/formulaire/formInscriptionEntreprise.php
+++ vue/formulaire/formInscriptionEntreprise.php
@@ -1,18 +1,66 @@
-<center>
-  <form  class="form-group" action="" method="post">
-    <table>
-      <tr><td>Nom</td><td><input  class="form-control" type="text" name="nom"></td></tr>
-      <tr><td>Prenom</td><td><input  class="form-control" type="text" name="prenom"></td></tr>
-      <tr><td>Email</td><td><input   class="form-control"type="text" name="email"></td></tr>
-      <tr><td>Mdp</td><td><input  class="form-control" type="password" name="mdp"></td></tr>
-      <tr><td>Telephon</td><td><input  class="form-control" type="text" name="telephone"></td></tr>
-      <tr><td>Date de naissance</td><td><input  class="form-control" type="date" name="date_naissance"></td></tr>
-      <tr><td>Adresse</td><td><input  class="form-control" type="text" name="adresse"></td></tr>
-      <tr><td>Code postal</td><td><input   class="form-control" type="text" name="code_postal"></td></tr>
-      <tr>
-                  <td><input class ='btn btn-primary'  type="submit" name="sinscrire" value="S'inscrire"></td>
-                  <td><input class ='btn btn-primary' type="reset" name="Annuler" value="Annuler"></td>
-      </tr>
-    </table>
-  </form>
-  </center>
+<div id="signin_div">
+
+<form action="" method="post">
+<div class="field">
+    <label for="username">Username</label>
+    <input type="text" name="username" id="username" autocomplete="off">
+</div>
+
+<div class="field">
+    <label for="password">Password</label>
+    <input type="password" name="password" id="password" autocomplete="off">
+</div>
+
+<div class="field">
+    <label for="remember">
+    <input type="checkbox" name="remember" id="remember"> Remember me
+    </label>
+</div>
+<input type="submit" value="Log in" class="login__submit">
+<p class="login__signup">Don't have an account? &nbsp;<a href="javascript:void(0)" id="showSignUp">Sign up</a></p>
+</form>
+
+</div>
+
+<div id="signup_div">
+
+<form action="" method="post">
+    <div class="field">
+        <label for="username">Username</label>
+        <input type="text" name="username" id="username" value="<?php  echo escape(Input::get('username')); //this is for sticky form ?>" autocomplete="off">
+    </div>
+
+    <div class="field">
+        <label for="password">Password</label>
+        <input type="password" name="password" id="password">
+    </div>
+
+    <div class="field">
+        <label for="password_again">Password again</label>
+        <input type="password" name="password_again" id="password_again">
+    </div>
+
+    <div class="field">
+        <label for="name">Name</label>
+        <input type="name" name="name" id="name" value="<?php echo escape(Input::get('name')); ?>">
+    </div>
+
+    <input type="hidden" name="token" value="<?php echo Token::generate(); ?>">
+    <input type="submit" value="Register">
+</form>
+
+</div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
+<script>
+
+$(document).ready(function(){
+    $("#showSignUp").click(function(){
+        $("#signin_div").hide();
+        $("#signup_div").show();
+
+    });
+});
+
+</script>
