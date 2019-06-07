ed8671eb431d53e40f6132c2a09b0a7a0c84fbff
kevin9499 <46714122+kevin9499@users.noreply.github.com>
2019-06-08 00:12:51 +0200

    Add files via upload

--- vue/formulaire/formCom.php
+++ vue/formulaire/formCom.php
@@ -1,18 +1,7 @@
 <form class="form-group" method="post" action="">
     <table border=0>
-    <tr> <td>
-            <?php
-        /*1
-                foreach( $lesCategs as $uneCateg)
-                {                        
-                    echo $uneCateg['designation'];
-                    break;
-                }
-                
-           */ ?>
-         </td>
-
-        <td class="td1">Note du poids</td>
+    <tr>
+        <td class="td1">Note</td>
             <td><select class="form-control" name="score">
                 <option value="1">1</option>
                 <option value="2">2</option>
@@ -21,53 +10,12 @@
                 <option value="5">5</option>
             </select></td>
         </tr>
-
-
-        <tr> <td>
-            <?php /*
-        
-                foreach( $lesCategs1 as $uneCateg)
-                {                        
-                    echo $uneCateg['designation'];
-                    break;
-                } 
-          */ ?>
-        </td>
-        <td class="td1">Note de la taille ecran</td>
-            <td><select class="form-control" name="score">
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-            </select></td>
-        </tr>
-        <tr> <td>
-            <?php /*
-        
-                foreach( $lesCategs2 as $uneCateg)
-                {                        
-                    echo $uneCateg['designation'];
-                    break;
-                }
-                */
-            ?>
-        </td>
-        <td class="td1">Note de la couleur</td>
-            <td><select class="form-control" name="score">
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-            </select></td>
-        </tr>        
-
+        <tr>      
     </table>
     <div class="col-sm-6">
         <p class = "pcom">Commentaire </p>
         <textarea class="form-control"  type="text" name="commentaire"></textarea></div>
         <input class ='buttonCom btn btn-primary' type="submit" name="Valider" value="Valider">
+        <input type="hidden" name="id_produit" value="<?php echo $id_produit ;?>">
         <input class ='buttonCom btn btn-primary' type="submit" name="Modifier" value="Modifier">
- 
 </form>
