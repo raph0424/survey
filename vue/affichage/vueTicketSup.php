6ffa871723408302d6083f5fff2073e1f58608ac
kevin9499 <46714122+kevin9499@users.noreply.github.com>
2019-06-12 15:17:08 +0200

    Add files via upload

--- vue/affichage/vueTicketSup.php
+++ vue/affichage/vueTicketSup.php
@@ -5,122 +5,68 @@
 <?php
 $id_partenaire = $_SESSION['id_partenaire'];
 $result = $unControleur->selectPartenaire();
+if(isset($_POST['Supprimer']))
+{
+    $id_ticket = $_POST['hidden'];
+    $envoi = array ( "id_ticket"=>$id_ticket);
+    $unControleur->delete("ticket",$envoi);
+}
 
-$result1 = $unControleur->selectTicketSup($id_partenaire);
 ?>
+
 <div class="col-sm-6">
 <?php
+$result1 = $unControleur->selectTicketSup($id_partenaire);
+
 foreach ($result1 as $unResultat)
 {
     echo"<table class='table table-bordered'>";
     echo" <tr><th width='40%'>Message de ".$unResultat['auteur']."</th><th width='10%'>Objet</th><th width='10%'> Date</th><th width='10%'>Supprimer message</th></tr>";       
     echo "<tr><td width='40%'>".$unResultat['contenu']."</td><td width='10%'>".$unResultat['objet']."</td><td width='10%'>".$unResultat['date']."</td>
-    <input type='hidden' name='hidden' value='".$unResultat['id_ticket']."'><td>
+    <td>
     <form class='form-group' action='' method='post'>
          <table>
+    <input type='hidden' name='hidden' value='".$unResultat['id_ticket']."'>
     <input class ='buttonCom btn btn-primary' type='submit' name='Supprimer' value='Supprimer'></td>
+    </br>
+    <input class ='buttonCom btn btn-primary' type='submit' name='Repondre' value='Répondre'></td>
     </table>
          </form></tr>";   
 
 }
 
+
 echo "</table>"; 
 ?>
 </div>
 <?php
-require_once("formulaire/formTicket.php");
 
-if(isset($_POST["envoyer"]))
+?>
+<form class="form-group" method="post" action="">
+    <table border=0>
+    <tr>
+        <td class="td1">Note</td>
+            <td><select class="td1 form-control" name="score">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+            </select></td>
+        </tr>
+        <tr>      
+    </table>
+    <div class="col-sm-6">
+        <p class = "pcom">Commentaire </p>
+        <textarea class="form-control"  type="text" name="commentaire"></textarea></div>
+        </br>
+        <input class ='buttonCom btn btn-primary' type="submit" name="<?php echo $valider_produit; ?>" value="Valider">
+        <input type="hidden" name="id_telephone" value="<?php echo $id_produit ;?>">
+        <input class ='buttonCom btn btn-primary' type="submit" name="Modifier" value="Modifier">
+</form>
+<style>
+.buttonCom 
 {
-   $envoi = array ("objet"=>$_POST['objet'], 
-   "date"=>$_POST['date'],
-   "contenu"=>$_POST['contenu'],
-   "id_personne"=>$_SESSION['id_personne'],
-   "id_partenaire"=>$_POST['id_partenaire']
-  );
-   $unControleur->insert("ticket",$envoi);
-  
-   echo '<div class="modal fade show" style="display : block;" id="ignismyModal" role="dialog">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true" onClick = "closeModal()"><span>×</span></button>
-                        </div>
-                        <div class="modal-body">
-                            <div class="thank-you-pop">
-                                <img src="http://goactionstations.co.uk/wp-content/uploads/2017/03/Green-Round-Tick.png" alt="">
-                                <h1>Merci!</h1>
-                                <p>Votre message bien été pris en compte, vous recevrez une réponse d\'ici 2 jours !</p>
-                            </div> 
-                        </div>
-                    </div>
-                </div>
-            </div>
- ';}?>
-
-<script>
-    function closeModal()
-    {
-        var modal = document.getElementById("ignismyModal");
-        modal.style.display = "none";
-    }
-</script>
-<style> 
-    /*--thank you pop starts here--*/
-    .thank-you-pop{
-        width:100%;
-        padding:20px;
-        text-align:center;
-    }
-    .thank-you-pop img{
-        width:76px;
-        height:auto;
-        margin:0 auto;
-        display:block;
-        margin-bottom:25px;
-    }
-
-    .thank-you-pop h1{
-        font-size: 42px;
-        margin-bottom: 25px;
-        color:#5C5C5C;
-    }
-    .thank-you-pop p{
-        font-size: 20px;
-        margin-bottom: 27px;
-        color:#5C5C5C;
-    }
-    .thank-you-pop h3.cupon-pop{
-        font-size: 25px;
-        margin-bottom: 40px;
-        color:#222;
-        display:inline-block;
-        text-align:center;
-        padding:10px 20px;
-        border:2px dashed #222;
-        clear:both;
-        font-weight:normal;
-    }
-    .thank-you-pop h3.cupon-pop span{
-        color:#03A9F4;
-    }
-    .thank-you-pop a{
-        display: inline-block;
-        margin: 0 auto;
-        padding: 9px 20px;
-        color: #fff;
-        text-transform: uppercase;
-        font-size: 14px;
-        background-color: #8BC34A;
-        border-radius: 17px;
-    }
-    .thank-you-pop a i{
-        margin-right:5px;
-        color:#fff;
-    }
-    #ignismyModal .modal-header{
-        border:0px;
-    }
-    /*--thank you pop ends here--*/
+    margin-bottom : 5px;
+}
 </style>
-
