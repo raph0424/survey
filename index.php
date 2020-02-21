d5f74025c5b493268dc52b1517487c6ee9b6785a
raph0424 <46817892+raph0424@users.noreply.github.com>
2020-02-21 16:18:07 +0100

    Update index.php

--- index.php
+++ index.php
@@ -60,6 +60,7 @@ if(isset($_SESSION['mdp']))
         <ul class="nav-menu">
           <li class="menu-active"><a href="index.php">Accueil</a></li>
           <li><a href="<?php echo $serie; ?>">J'ai regardé une Série</a></li>
+	  <li><a href="<?php echo $film; ?>">J'ai regardé un Film</a></li>	
 		  <li><a href="<?php echo $panel; ?>"><?php echo $np; ?></a></li>
           <li><a href="<?php echo $linkCon; ?>"><?php echo $connec; ?></a></li>
           <li><a href="<?php echo $sign; ?>"><?php echo $signe; ?></a></li>
